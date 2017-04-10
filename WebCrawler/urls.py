b1013e02218be9f5f5ad3c5ecce8dd020f2854dd
Kalaiarasan <kalaiarasan.saminathan@aalto.fi>
2017-04-10 15:35:02 +0300

    COde changes for Webhook

--- WebCrawler/urls.py
+++ WebCrawler/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^store/', views.store_data),
+    url(r'^store/', views.store_data, name="store_data"),
+    url(r'^store_webhook/', views.store_data_web_hook, name="store_data_web_hook"),
 ]
--- WebCrawler/views.py
+++ WebCrawler/views.py
@@ -1,12 +1,32 @@
 from .models import ApplicationUser, MessageHolder
 import json
 import requests
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from datetime import datetime
-import time
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 TOKEN = "367274256:AAFh1eeLfF8QIqC7XH0KcoR4pIIK7o_7Y_k"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+@csrf_exempt
+@require_POST
+def store_data_web_hook(request):
+    updates = json.loads(request.body)
+    if not ApplicationUser.objects.filter(user_id=updates["message"]["from"]["id"]).exists():
+        app_user = ApplicationUser(user_id=updates["message"]["from"]["id"],
+                                   first_name=updates["message"]["from"]["first_name"],
+                                   username=updates["message"]["from"]["username"])
+        app_user.save()
+
+    app_user = ApplicationUser.objects.get(user_id=updates["message"]["from"]["id"])
+    message = MessageHolder(user_id=app_user,
+                            message=updates["message"]["text"],
+                            message_id=updates["message"]["message_id"],
+                            message_date=datetime.fromtimestamp(updates["message"]["date"]))
+    message.save()
+    return HttpResponse(status=200)
 
 
 def store_data(request):
@@ -41,12 +61,3 @@ def get_data(url):
 def get_url(url):
     response = requests.get(url)
     return response.content.decode("utf8")
-
-
-def get_last_chat_id_and_text(updates):
-    print(updates)
-    num_updates = len(updates["result"])
-    last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]
-    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
