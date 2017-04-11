2709556637067f67f5a785e633e553497ba12124
Kalaiarasan <kalaiarasan.saminathan@aalto.fi>
2017-04-11 18:30:26 +0300

    Throttling for Rest APIs

--- TelegramChatBot/settings.py
+++ TelegramChatBot/settings.py
@@ -131,3 +131,14 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '10000/day'
+    }
+}
