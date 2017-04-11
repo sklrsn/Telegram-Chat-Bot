11e8ca52e70603ebc262118aed1dcae8b0b6fc99
Kalaiarasan <kalaiarasan.saminathan@aalto.fi>
2017-04-12 01:52:26 +0300

    Unit Test cases

--- Explorer/tests.py
+++ Explorer/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
+import json
 
-# Create your tests here.
+
+class ExplorerTestCases(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_exploreByKeyword(self):
+        exploreKeywordResponse = self.client.post('http://127.0.0.1:8000/v1/search/exploreByKeyword/',
+                                                  json.dumps({"keyword": "Good"}),
+                                                  content_type='application/json')
+        self.assertEqual(exploreKeywordResponse.status_code, 200)
+
+    def test_exploreByDate(self):
+        exploreByDateResponse = self.client.post('http://127.0.0.1:8000/v1/search/exploreByDate/',
+                                                 json.dumps({"query_date": "2017/04/09"}),
+                                                 content_type='application/json')
+        self.assertEqual(exploreByDateResponse.status_code, 200)
+
+    def test_exploreByUserName(self):
+        exploreByUserNameResponse = self.client.post('http://127.0.0.1:8000/v1/search/exploreByUserName/',
+                                                     json.dumps({"username": ""}),
+                                                     content_type='application/json')
+        self.assertEqual(exploreByUserNameResponse.status_code, 400)
